07aee36ac95eafcb504f159f65b7635d48c22add
joyanta <joyanta.uk@gmail.com>
2021-08-10 08:04:43 +0100

    appying OnPush on results component

--- src/app/weather/components/results/results.component.ts
+++ src/app/weather/components/results/results.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
 import { Summary } from 'src/app/model';
 
 @Component({
   selector: 'app-results',
-  templateUrl: './results.component.html'
+  templateUrl: './results.component.html', 
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResultsComponent implements OnChanges {
   

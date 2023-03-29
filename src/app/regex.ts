ca42d2f8501485ae87d5b8e5c87d72ac62377ae2
Joyanta Raksmith <joyanta.uk@gmail.com>
2023-03-29 12:30:46 +0100

    stuff

--- src/app/regex.ts
+++ src/app/regex.ts
@@ -4,3 +4,23 @@
 
 
 ^[a-zA-Z0-9\s!@#$%^&*()_+=[\]{}|;':",./<>?`~\\-]+$
+
+
+import { HttpClient } from '@angular/common/http';
+
+const baseUrl = 'https://example.com/api';
+const params = {
+  param1: 'value1',
+  param2: 'value2'
+};
+
+// Encode parameters using encodeURIComponent()
+const encodedParams = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`).join('&');
+
+const url = `${baseUrl}?${encodedParams}`;
+
+this.http.get(url).subscribe((response) => {
+  // handle response
+}, (error) => {
+  // handle error
+});

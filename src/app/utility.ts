b8ba88849d34e5f5f49ee404e19698906d65b4eb
Joyanta Raksmith <joyanta.uk@gmail.com>
2023-04-17 07:13:00 +0100

    stuff

--- src/app/utility.ts
+++ src/app/utility.ts
@@ -4,4 +4,45 @@ function trimObjectValues(obj: {[key: string]: string}): {[key: string]: string}
       return acc;
     }, {});
   }
+
   
+
+ // https://subscription.packtpub.com/book/web-development/9781838989439/2/ch02lvl1sec14/creating-a-basic-directive-that-allows-you-to-vertically-scroll-to-an-element
+
+
+
+//  ng generate directive <directive-name>
+
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+@Directive({
+  selector: '[appSticky]'
+})
+export class StickyDirective {
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
+  onWindowScroll() {
+    const element = this.elementRef.nativeElement;
+    const offset = element.offsetTop;
+    const scrollPosition = window.pageYOffset;
+
+    if (scrollPosition >= offset) {
+      this.renderer.addClass(element, 'sticky');
+    } else {
+      this.renderer.removeClass(element, 'sticky');
+    }
+  }
+}
+
+
+ngAfterViewInit() {
+  const element = this.elementRef.nativeElement;
+
+  this.renderer.listen(element, 'scroll', () => {
+    this.onWindowScroll();
+  });
+}
+
+
+<div appSticky>
+  <!-- Content -->
+</div>

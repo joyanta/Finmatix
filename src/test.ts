4708f4d94eec7e6d89a20d1aa1b6e4399a3586fc
Joyanta Raksmith <joyanta.uk@gmail.com>
2024-08-08 10:13:55 +0100

    changes

--- src/test.ts
+++ src/test.ts
@@ -23,3 +23,53 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
+
+
+
+
+
+////
+
+
+public class BearerTokenMessageInspector : IClientMessageInspector
+{
+    private readonly string _token;
+
+    public BearerTokenMessageInspector(string token)
+    {
+        _token = token;
+    }
+
+    public void AfterReceiveReply(ref Message reply, object correlationState) { }
+
+    public object BeforeSendRequest(ref Message request, IClientChannel channel)
+    {
+        var httpRequestMessage = new HttpRequestMessageProperty();
+        httpRequestMessage.Headers["Authorization"] = $"Bearer {_token}";
+        request.Properties[HttpRequestMessageProperty.Name] = httpRequestMessage;
+        return null;
+    }
+}
+///
+
+
+public class BearerTokenEndpointBehavior : IEndpointBehavior
+{
+    private readonly string _token;
+
+    public BearerTokenEndpointBehavior(string token)
+    {
+        _token = token;
+    }
+
+    public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+
+    public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+    {
+        clientRuntime.ClientMessageInspectors.Add(new BearerTokenMessageInspector(_token));
+    }
+
+    public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher) { }
+
+    public void Validate(ServiceEndpoint endpoint) { }
+}
